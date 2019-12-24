17c08ba4dd903e38b226c969a6a60d9eafe0a96b
armonkahil <armonkahil@gmail.com>
2019-12-24 00:04:06 -0500

    deployed to heroku

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require('mongoose')
+const gradient = require('gradient-string')
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/googleBooks'
+// Mongoose Deprecation warnings disabled
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+mongoose.connect(MONGODB_URI)
+
 
 // Send every request to the React app
 // Define any API routes before this runs
