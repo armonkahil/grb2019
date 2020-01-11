33aa49155e3dda390899be001cc1a4312c29f59c
armonkahil <armonkahil@gmail.com>
2020-01-11 10:07:07 -0500

    heroku fix

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -14,7 +14,8 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
   },
-  create: function(req, res) {
+  create: function (req, res) {
+    console.log('create route hit')
     db.Book.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err))
--- server.js
+++ server.js
@@ -11,6 +11,26 @@ console.log('MONGODB_URI:', MONGODB_URI)
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888')
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  )
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+
+  // Pass to next layer of middleware
+  next()
+})
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
@@ -21,7 +41,10 @@ app.use(routes)
 // Connect to the Mongo DB
  mongoose.Promise = global.Promise
 //  console.log('global:', global.Promise)
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true})
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+// mongoose.set('useCreateIndex', true)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true })
 mongoose.connection.on('error', err => {
   logError(err)
 })
